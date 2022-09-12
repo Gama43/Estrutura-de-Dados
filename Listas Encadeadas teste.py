c59fd53c3ee2a6b4fdd21d2542af2e51be2d9d89
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-09-12 12:17:35 -0300

    Add files via upload

---
+++ Listas Encadeadas teste.py
@@ -0,0 +1,73 @@
+def solveJosephus (n, m):
+    n = int(input())
+    m = int(input())
+
+class Pessoa:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:  
+    def __init__(self):
+        self.head = Pessoa()
+
+    def append(self, data):
+        nova_pessoa = Pessoa(data)
+        atual = self.head
+        while atual.next != None:
+            atual = atual.next
+        atual.next = nova_pessoa
+    
+    def tamanho(self):
+        atual = self.head
+        total = 0
+        while atual.next != None:
+            total += 1
+            atual = atual.next
+        return total
+    
+    def display(self):
+        elems = []
+        atual_pessoa = self.head
+        while atual_pessoa.next !=None:
+            atual_pessoa = atual_pessoa.next
+            elems.append(atual_pessoa.data)
+        print (elems)
+
+    def get(self, index):
+        if index >= self.tamanho():
+            return None
+        atual_idx = 0
+        atual_pessoa = self.head
+        while True:
+            atual_pessoa = atual_pessoa.next
+            if atual_idx == index: return atual_pessoa.data
+            atual_idx += 1
+
+    def remove(self, index):
+        if index >= self.tamanho():
+            return 
+        atual_idx = 0
+        atual_pessoa = self.head
+        while True:
+            ultima_pessoa = atual_pessoa
+            atual_pessoa = atual_pessoa.next
+            if atual_idx == index:
+                ultima_pessoa.next = atual_pessoa.next
+                return
+            atual_idx += 1
+        
+
+
+
+l = LinkedList()
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+l.append(5)
+l.append(6)
+l.display()
+l.remove(1)
+l.display()
+
