3a02069d50fb4b3225b3e5812964053ffe0c14be
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-09-21 12:56:02 -0300

    Update Trabalho_1.py

--- Trabalho_1.py
+++ Trabalho_1.py
@@ -1,7 +1,5 @@
-
-
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
@@ -10,21 +8,48 @@ class CircularLinkedList:
         self.head = None
     
     def append(self, data):
-        node = Node(data)
-        self.inserir_final(node)
-    
-    def inserir_final(self, nova_node):
-        if self.head is None:
-            self.head = nova_node
-    
-    def printar(self):
+        new_node = Node(data)
+        atual = self.head
+        new_node.next = self.head
+        if not self.head:
+            new_node.next = new_node
+        else:
+            while atual.next != self.head:
+                atual = atual.next
+            atual.next = new_node
+        self.head = new_node
+
+    def print_list(self):
+        atual = self.head
+        while atual:
+            print(atual.data)
+            atual = atual.next
+            if atual == self.head:
+                break
+
+    def remove(self, key):
+        if self.head.data == key:
+            atual = self.head
+            while atual.next != self.head:
+                atual = atual.next
+            atual.next = self.head.next
+            self.head = self.head.next
+        else:
+            atual = self.head
+            prev = None
+            while atual.next != self.head:
+                prev = atual
+                atual = atual.next
+                if atual.data == key:
+                    prev.next = atual.next
+                    atual = atual.next
+
+    def josephus(self):
         pass
 
 
+n = 10
 l = CircularLinkedList()
-n = int(input())
-m = int(input())
-
-
-for i in range(1, n + 1):
-    l.append(i)
+for i in range(n, 0, -1):
+    l.append(i) 
+l.print_list()
