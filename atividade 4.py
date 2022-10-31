083a4e917b796b4106b1b9cdb879135851a12b37
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-10-31 00:23:25 -0300

    Update atividade 4.py

--- atividade 4.py
+++ atividade 4.py
@@ -5,32 +5,40 @@ class Node:
         self.value = value
         self.left = None
         self.rigth = None
+    
 
-
-expressao = 'A-B+C*D*E'
 def verificar_operador(expressao):
-        i = len(expressao) - 1
-        while i >= 0:
-            x = expressao[i]
-            if x == '+' or x == '-':
-                return i
-               
-            elif x == '*' or x == '/':
+    i = len(expressao) - 1
+    while i >= 0:
+        x = expressao[i]
+        if x == '+' or x == '-':
+            return i
+
+        else:
+            if "+" not in expressao and "-" not in expressao:
+                if x == '*' or x == '/':
+
+                    return i
+        i -= 1
+def montando_arvore(expressao):
+    i = verificar_operador(expressao)
+    if i is not None:
+        raiz = expressao[i]
+        filho_esquerdo = expressao[0:i]
+        filho_direito = expressao[i + 1]
         
-                return i
-            i -= 1
+        root = Node(raiz)
+        root.left = Node(filho_esquerdo)
+        root.rigth = Node(filho_direito)
+
+        montando_arvore(filho_esquerdo)
+        montando_arvore(filho_direito)
+
+expressao = 'A*B+C*D-E'
+montando_arvore(expressao)
 
 
-i = verificar_operador(expressao)
-print(i)
-raiz = expressao[i]
-print(raiz)
-filho_esquerdo = expressao[0:i]
-filho_direito = expressao[i+1]
 
-root = Node(raiz)
-root.left = Node(filho_esquerdo)
-root.rigth = Node(filho_direito)
 
-#criar uma função que use o verificador, para construir a arvore
-#Separar em dois while, pra mais e menos e pra vezes e div
+
+
